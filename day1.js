a77e76fbb0d2158d1a454306f226f36b1d66c1ef
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2024-07-20 00:00:11 +0530

    day1 js file added.

---
+++ day1.js
@@ -0,0 +1,54 @@
+// Activity 1
+// TAsk 1
+var x = 1;
+console.log(x);
+
+// Task2
+let y = "VyomPundhir";
+console.log(y);
+
+// Activity 2
+// Task3
+const isBoolean = true;
+console.log(isBoolean);
+
+// Activity 3
+// Task4
+let a = 10;
+let str1 = "This is my String.";
+let boolean = true;
+let obj1 = {
+  Username: "vyompundhir",
+  Password: 8445198500
+};
+let arr1 = [12, "Vyom", true, {
+  name: "Yash",
+  class: 12,
+  isPassed: true
+}];
+
+console.log(a);
+console.log(typeof(a))
+console.log(str1);
+console.log(typeof(str1));
+console.log(boolean)
+console.log(typeof(boolean));
+console.log(obj1);
+console.log(typeof(obj1));//returns object
+console.log(arr1);
+console.log(typeof(arr1));//*****returns  object*******
+console.log(Array.isArray(arr1));//returns true
+
+// Activity4
+// Task5
+let value = 40;
+console.log(value)
+value= 60;
+console.log(value)
+
+// Activity5
+// Task6
+const constant = 200;
+console.log(constant);
+// constant= 300;  *Constants can not be reassigned after declaration.*
+// console.log(constant);
